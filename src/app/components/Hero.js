5f43ce98b59e69d8b52b200bde6a5df97d3c2b7c
slashMK303 <slashluck3@gmail.com>
2025-07-02 15:24:05 +0700

    Remove unnecessary blank line in Hero component

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -31,7 +31,6 @@ export default function Hero() {
                 </motion.svg>
             </div>
 
-
             <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-12 items-start">
 
                 <motion.div
