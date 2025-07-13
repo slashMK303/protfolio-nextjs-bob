01a9239d7e3e88526a5228cb51a476a4d405ecb2
slashMK303 <slashluck3@gmail.com>
2025-07-13 11:43:52 +0700

    Update metadata in layout.js and sitemap.xml for consistency and improved SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,21 +4,21 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Nanang Marvin - Web Developer',
+  title: 'Nanang Marvin Kurniawan',
   description: 'Portfolio of Nanang Marvin Kurniawan, a Web Developer focused on building innovative digital products.',
   keywords: 'web developer, web designer, frontend, backend, UI/UX, Next.js, React, Node.js, Tailwind CSS, full stack, Nanang Marvin Kurniawan, Nanang, Nanang Marvin',
 
   openGraph: {
-    title: 'Nanang Marvin - Web Developer',
+    title: 'Nanang Marvin Kurniawan',
     description: 'Explore the portfolio of Nanang Marvin Kurniawan, a creative Web Developer with expertise in full-stack development.',
     url: 'https://nanangmarvin.my.id',
-    siteName: 'Nanang Marvin',
+    siteName: 'Nanang Marvin Kurniawan',
     images: [
       {
         url: 'https://nanangmarvin.my.id/img/hero.jpg',
         width: 1200,
         height: 630,
-        alt: 'Nanang Marvin - Web Developer',
+        alt: 'Nanang Marvin Kurniawan',
       },
     ],
     type: 'website',
@@ -26,7 +26,7 @@ export const metadata = {
 
   twitter: {
     card: 'summary_large_image',
-    title: 'Nanang Marvin - Web Developer',
+    title: 'Nanang Marvin Kurniawan',
     description: 'Portfolio of Nanang Marvin Kurniawan, Web Developer focused on building impactful digital experiences.',
     images: ['https://nanangmarvin.my.id/img/hero.jpg'],
   },
